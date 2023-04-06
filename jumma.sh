d846bd4df862bc4d257980565146887a22a321d6
jumma <jumma.ahmed87@gmail.com>
2023-04-06 17:06:13 +0100

    error-messages

--- jumma.sh
+++ jumma.sh
@@ -1,23 +1,33 @@
-#/bin/bash
+#!/bin/bash
 #jumma.sh THIS SCRIPT ALLOWS THE USER TO BACKUPS FILES AND FOLER BY PASSING A PARAMETER COMMAND
-function backupme(){
-    # CREATE A DESTINATION FILE UISNG THE CURRENT TIME STAMP
-    timeStamp='date +%y%m%d-%H%M%S'
-    destinationFleName='biotech'-${timeStamp}.tar
-    # GET THE CURRENT NUMBER OF ARGUMENTS PASSED 
-    argumentsNumber=$#
-    # IF THE ARGUMENTS DOES NOT MATCH THE DESIRED NUMBER
-    if [$argumentsNumber -lt 1] ; then
-        # LET THE USER KNOW THAT THE ARGUMENTS ARE NOT CORRECT 
-        echo 'You need to input a argument when running the script!!' 
-        # CLOSE THE SCRIPT
+
+echo "Welcome to our backup script, please type backmeup and what type op backup you would like to do"
+echo "1 = local backup"
+echo "2 = network backup"
+echo "Here is an example of how to execute the code 'backmeup {option}'. "
+
+# THE FUNCTION THAT WILL ALLOW THE USER TO BACUP FILES
+function backmeup() { 
+    # VARIABLE THAT WILL HOLD THE TOT NUMBER OF PARAMETERS PASSED 
+    parameterCounter=$#
+    # MAKE SURE THAT THE CORRECT NUMBER OF PARAMETERS HAVE BEEN PASSED 
+    if ((parameterCounter < 1)); then
+        # LET THE USER KNOW THAT THE NUMBER OF PARAMETERS IS INCORRECT
+        echo "ERROR: No option has been declared (1, 2), for more help try 'backmeup-help'."
         exit 1
     fi
-
-    # STORE THE OPTION PASSED TO SRC
-    src=${1}
-    # CREATE A ITERATIVE VARIABLE THAT WIL LOOP TRHOUGH
-    i=1
+    # IF CORRECT NUMBER OF PARAMETER HAS BEEN GIVEN THEN STORE THE VALUE
+    selectedOption=${1}
+    # IF OPTION 1 WAS SELECTED
+    if ((selectedOption == 1)); then
+        echo "You have selected option 1!"
+    fi
+    # IF OPTION 2 WAS SELECTED 
+    if ((selectedOption == 2)); then
+        echo "You have selected option 2!"
+    fi
+    # IF THE OPTION SELECTED IS INVALID 
+    if ((selectedOption != 1 && selectedOption != 2)); then
+        echo "ERROR: The selected option is invalid, for more information type 'backmeup-help'."
+    fi
 }
-
-
