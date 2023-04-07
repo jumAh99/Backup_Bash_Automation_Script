accafac56f4058667029b07fb6c0ed6ca0fda420
jumma <jumma.ahmed87@gmail.com>
2023-04-07 16:06:17 +0100

    changes to folder structure and taking out exit

--- jumma.sh
+++ jumma.sh
@@ -9,16 +9,15 @@ function backmeup() {
     # VARIABLE THAT WILL HOLD THE TOT NUMBER OF PARAMETERS PASSED 
     parameterCounter=$#
     # GET THE TIME AND DATE FOR ORGANISATION FOR BACKUPS 
-    timeStamp=$(date +"%Y-%m-%d-%H:%M:%S")
+    timeStamp=$(date +"%Y-%m-%d_%H:%M:%S")
     # SYSTEM CURRENT LOCATION 
-    locationPath=/home/bob/Documents/scripts
+    locationPath=/home/bob/Documents/scripts/target
     # DESTINATION OF WHERE THE ARCHIVES WILL BE STORED 
-    destinationFile=$locationPath/backups/biotech$timeStamp.tar
+    destinationFile=$locationPath/backups/biotech_$timeStamp.tar
     # MAKE SURE THAT THE CORRECT NUMBER OF PARAMETERS HAVE BEEN PASSED 
     if ((parameterCounter < 1)); then
         # LET THE USER KNOW THAT THE NUMBER OF PARAMETERS IS INCORRECT
         echo "ERROR: No option has been declared (1, 2), for more help try 'backmeup-help'."
-        exit 1
     fi
     # IF CORRECT NUMBER OF PARAMETER HAS BEEN GIVEN THEN STORE THE VALUE
     selectedOption=${1}
@@ -26,13 +25,16 @@ function backmeup() {
     if ((selectedOption == 1)); then
         # GET THE USER DIRECTORY PATH WHERE THE FILES ARE STORED 
         userDirectory=$(ls $locationPath)
-        # LOOP THROUGH ALL THE FILES PRESENT IN THE USER FOLDER
+        # LOOP TROUGH ALL THE FILES AND STORE THEM IN A VARIABLE NAMED ENTRY
         for entry in $userDirectory; do
             # MAKE SURE TO GRAB ONLY FILES AND DIRECTORIES /home/bob/Documents/scripts/
             if [ -d "$entry" ] || [ -f "$entry" ]; then
                 # STORE ALL THE FILTERED FILES INTO A SOURCE FILE
                 sourceToBackup="${entry}"
+                # CREATE AN ARCHIVE WITH THE FILES THAT ARE VALID FOR THE BACKUP 
+                tar -cvf "$destinationFile" "$sourceToBackup"
             fi
+            echo "value of source is:$entry"
         done
     fi
     # IF OPTION 2 WAS SELECTED 
@@ -43,9 +45,6 @@ function backmeup() {
     if ((selectedOption != 1 && selectedOption != 2)); then
         echo "ERROR: The selected option is invalid, for more information type 'backmeup-help'."
     fi
-
-    # CREATE AN ARCHIVE WITH THE FILES THAT ARE VALID FOR THE BACKUP 
-    tar -cvf "$destinationFile" "$sourceToBackup"
 }
 # ALLOW THE USER TO ACCESS USAGE INFORMATION AT ANY TIME 
 function bakmeup-help(){
