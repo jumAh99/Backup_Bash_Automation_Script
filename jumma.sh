d118fb95655ae5ea21ba2b9a77827748c4de0e28
jumma <jumma.ahmed87@gmail.com>
2023-04-08 15:25:47 +0100

    correctly storing files o the right place

--- jumma.sh
+++ jumma.sh
@@ -11,9 +11,7 @@ function backmeup() {
     # GET THE TIME AND DATE FOR ORGANISATION FOR BACKUPS 
     timeStamp=$(date +"%Y-%m-%d_%H:%M:%S")
     # SYSTEM CURRENT LOCATION 
-    locationPath=/home/bob/Documents/scripts/target
-    # DESTINATION OF WHERE THE ARCHIVES WILL BE STORED 
-    destinationFile=$locationPath/backups/biotech_$timeStamp.tar
+    locationPath=/home/bob/Documents/scripts
     # MAKE SURE THAT THE CORRECT NUMBER OF PARAMETERS HAVE BEEN PASSED 
     if ((parameterCounter < 1)); then
         # LET THE USER KNOW THAT THE NUMBER OF PARAMETERS IS INCORRECT
@@ -21,25 +19,52 @@ function backmeup() {
     fi
     # IF CORRECT NUMBER OF PARAMETER HAS BEEN GIVEN THEN STORE THE VALUE
     selectedOption=${1}
+    # GET THE USER DIRECTORY PATH WHERE THE FILES ARE STORED 
+    listOfContentInTargetFolder=$(ls $locationPath/target)
+    
     # IF OPTION 1 WAS SELECTED
     if ((selectedOption == 1)); then
-        # GET THE USER DIRECTORY PATH WHERE THE FILES ARE STORED 
-        userDirectory=$(ls $locationPath)
+        # DESTINATION OF WHERE THE ARCHIVES WILL BE STORED 
+        destinationFile=/home/bob/Documents/scripts/local-drive/biotech_$timeStamp.tar
         # LOOP TROUGH ALL THE FILES AND STORE THEM IN A VARIABLE NAMED ENTRY
-        for entry in $userDirectory; do
-            # MAKE SURE TO GRAB ONLY FILES AND DIRECTORIES /home/bob/Documents/scripts/
-            if [ -d "$entry" ] || [ -f "$entry" ]; then
-                # STORE ALL THE FILTERED FILES INTO A SOURCE FILE
-                sourceToBackup="${entry}"
-                # CREATE AN ARCHIVE WITH THE FILES THAT ARE VALID FOR THE BACKUP 
-                tar -cvf "$destinationFile" "$sourceToBackup"
+        for entry in $listOfContentInTargetFolder; do
+            if [ "$entry" == "usr" ] && [ "$entry" != "etc" ]; then
+                # IF ENTRY IS A FILE OR DIRECTORY
+                if [ -d "$locationPath/target/$entry" ] || [ -f "$locationPath/target/$entry" ]; then
+                    # STORE ALL THE FILTERED FILES INTO A SOURCE FILE
+                    sourceToBackup="${entry}"
+                    # CREATE AN ARCHIVE WITH THE FILES THAT ARE VALID FOR THE BACKUP 
+                    tar -cvf "$destinationFile" "target/$sourceToBackup"
+                    # LET THE USER KNOW THAT THE FILE INDICATED IS NOT A FILE OR DIRECTORY
+                    else
+                        # PRINT A ERROR MESSAGE
+                        echo "ERROR: $locationPath/target/$entry is not a file or directory!!"
+                fi
             fi
-            echo "value of source is:$entry"
         done
     fi
     # IF OPTION 2 WAS SELECTED 
     if ((selectedOption == 2)); then
-        echo "You have selected option 2!"
+        # DESTINATION OF WHERE THE ARCHIVES WILL BE STORED 
+        destinationFile=/home/bob/Documents/scripts/network-drive/biotech_$timeStamp.tar
+        # LOOP TROUGH ALL THE FILES AND STORE THEM IN A VARIABLE NAMED ENTRY
+        for entry in $listOfContentInTargetFolder; do
+            if [ "$entry" == "etc" ] && [ "$entry" != "usr" ]; then
+                # IF ENTRY IS A FILE OR DIRECTORY
+                if [ -d "$locationPath/target/$entry" ] || [ -f "$locationPath/target/$entry" ]; then
+                    # STORE ALL THE FILTERED FILES INTO A SOURCE FILE
+                    sourceToBackup="$entry"
+                    # CREATE AN ARCHIVE WITH THE FILES THAT ARE VALID FOR THE BACKUP 
+                    tar -cvf "$destinationFile" "target/$sourceToBackup"
+                    # LET THE USER KNOW THAT THE FILE INDICATED IS NOT A FILE OR DIRECTORY
+                    else
+                        # PRINT A ERROR MESSAGE
+                        echo "ERROR: $locationPath/target/$entry is not a file or directory!!"
+                fi
+                else
+                    echo "ERROR: $entry"
+            fi
+        done
     fi
     # IF THE OPTION SELECTED IS INVALID 
     if ((selectedOption != 1 && selectedOption != 2)); then
@@ -47,7 +72,7 @@ function backmeup() {
     fi
 }
 # ALLOW THE USER TO ACCESS USAGE INFORMATION AT ANY TIME 
-function bakmeup-help(){
+function backmeup-help(){
     echo "Welcome to our backup script, please type backmeup and what type of backup you would like to do"
     echo "1 = local backup"
     echo "2 = network backup"
